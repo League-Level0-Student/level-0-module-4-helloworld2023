9367ae80712464707dc7e24ee8db1163d2adb119
league <league@wts13.attlocal.net>
2018-10-07 13:00:34 -0700

    I started Mod4

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,17 +15,20 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String number = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-
+int nickels = Integer.parseInt(number);
 		// Ask the user how many dimes they have, and convert their answer
-
+String number2 = JOptionPane.showInputDialog("How many dimes do you have?");
 		// Ask the user how many quarters they have, and convert their answer
-
+int dimes = Integer.parseInt(number2);
 		// Calculate how much money the user has and save it in a double variable 
-
+double first_number, second_number, answer ;
+first_number = nickels;
+second_number = dimes;
+answer = first_number + second_number;
 		// Tell the user how much money they have
-
+JOptionPane.showMessageDialog(null, "The total amount of money you have is $" + answer);
 	}
 }
 
